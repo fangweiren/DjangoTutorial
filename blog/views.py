481bfb63b059cb17ca32d0db30f600077bf31b13
fangweiren <fwr843@163.com>
2017-12-07 23:30:41 +0800

    修改模板

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from .models import Post
 
 # Create your views here.
 def index(request):
+	#post_list = Post.objects.all()
     return HttpResponse(u"欢迎光临 我的博客!")
+	#return render(request, 'index.html', context={'post_list': post_list})
