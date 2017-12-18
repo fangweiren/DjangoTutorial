3ceab95401e37dec4a8613cf54b71c6fceb62f39
fangweiren <fwr843@163.com>
2017-12-18 10:18:36 +0800

    Django 博客首页

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-	post_list = Post.objects.all()
+	post_list = Post.objects.all().order_by('-create_time')
     #return HttpResponse(u"欢迎光临 我的博客!")
 	return render(request, 'index.html', context={'post_list': post_list})
