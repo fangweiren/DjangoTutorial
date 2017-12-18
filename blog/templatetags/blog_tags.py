a4c0334dc39744f151df24d1b452fd3309b96947
fangweiren <fwr843@163.com>
2017-12-18 23:15:21 +0800

    页面侧边栏：自定义模板标签

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
+
+register = template.Library()
+
+@register.simple_tag
+def get_reading_posts(num=5):
+	return Post.objects.all().order_by('-read_amount')[:num]
+
+@register.simple_tag
+def archives():
+    return Post.objects.datetimes('create_time', 'month', order='DESC')
+	
+	
+@register.simple_tag
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    #return Category.objects.all()
+	return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag	
+def get_tag():
+	return Tag.objects.annotate(num_posts=Count('post'))
