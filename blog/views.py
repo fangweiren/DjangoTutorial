a875ccd736136c93129d1a79061fba1532cc9267
fangweiren <fwr843@163.com>
2017-12-18 13:23:45 +0800

    Django 博客文章详情页

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -7,3 +7,7 @@ def index(request):
 	post_list = Post.objects.all().order_by('-create_time')
     #return HttpResponse(u"欢迎光临 我的博客!")
 	return render(request, 'index.html', context={'post_list': post_list})
+	
+def post_detail(request, post_id):
+	post = get_object_or_404(Post, id=post_id)
+	return render(request, 'detail.html', context={'post': post})
--- myblog/urls.py
+++ myblog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from blog.views import index
+from blog.views import index, post_detail
 
 urlpatterns = [
 	url(r'^$', index, name='index-url'),
+	url(r'^post/(\d+)/$', post_detail, name='post_detail-url'),
     url(r'^admin/', admin.site.urls),
 ]
