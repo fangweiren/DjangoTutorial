d0a914d9f48c8774b696479e31ec4133932c170e
fangweiren <fwr843@163.com>
2017-12-19 23:07:01 +0800

    统计各个分类下的文章数

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,13 +10,16 @@ def get_reading_posts(num=5):
 
 @register.simple_tag
 def archives():
-    return Post.objects.datetimes('create_time', 'month', order='DESC')
-	
-	
+	return Post.objects.datetimes('create_time', 'month', order='DESC')
+
+@register.filter
+def get_archives_num(date):
+	return Post.objects.filter(create_time__year=date.year, create_time__month=date.month).count()	
+
 @register.simple_tag
 def get_categories():
-    # 别忘了在顶部引入 Category 类
-    #return Category.objects.all()
+	# 别忘了在顶部引入 Category 类
+	#return Category.objects.all()
 	return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
 @register.simple_tag	
